9136ca9409819ae52f123cfb747adfbddf2bfd83
Tanaka <tanakakenne2000@gmail.com>
2022-12-12 13:55:43 +0200

    added placeholder image

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,32 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
-  tags = [
-    'Frontend',
-    'Angular',
-    'Personal',
-    'Professional'
-  ];
-
   projects: Project[] = [
-    {
-      name: 'Assembly Management',
-      description: 'platform for the AFM Church in the Gauteng Region that attempts to modernize and/or digitize the existing manual management system currently being used by the church.',
-      tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Web', 'Node.js', 'Backend'],
-      platforms: ['Web']
-
-    },
     {
       name: 'Mentor Me',
       description: 'This is a mobile application that seeks to provide value through mentorship.',
       imageSrc: 'mentor_me_preview',
-      tags: ['Frontend','Firebase', 'Mobile', 'Ionic', 'Node.js', 'Backend', 'Neo4j', 'Personal'],
-      platforms: ['Mobile']
+      tags: ['Frontend','Firebase', 'Ionic', 'Node.js', 'Backend', 'Neo4j', 'Personal'],
+      platforms: ['Mobile'],
+      sourceCode: 'https://github.com/tanaka-kenny/mentorMe'
+    },
+    {
+      name: 'Assembly Management',
+      description: 'platform for the AFM Church in the Gauteng Region that attempts to modernize and/or digitize the existing manual management system currently being used by the church.',
+      tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Node.js', 'Backend'],
+      platforms: ['Web']
+
     },
     {
       name: 'Horus Management System',
       description: 'remote asset monitoring platform for cell towers that provides clients with visibility of their entire asset portfolio',
-      tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Mobile', 'Ionic', 'Web', 'Node.js', 'Backend'],
+      tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Ionic', 'Node.js', 'Backend'],
       liveUrl: '',
       platforms: ['Web', 'Mobile']
     },
@@ -40,7 +34,7 @@ export class ProjectsComponent {
       name:'Kasi Connect',
       description: 'A mobile that attempted to bring internet connect to South African townships',
       imageSrc: 'kasi_connect_preview',
-      tags: ['Profession', 'Frontend', 'Angular','Firebase', 'Mobile', 'Ionic'],
+      tags: ['Profession', 'Frontend', 'Angular','Firebase', 'Ionic'],
       liveUrl: 'https://play.google.com/store/apps/details?id=za.co.omnipresent.kasi.connect&pli=1',
       platforms: ['Mobile']
     },
@@ -48,7 +42,7 @@ export class ProjectsComponent {
       name: 'Murder in Paris',
       description: 'A website for the multi-award winning documentary by South African director, Enver Samuel.',
       imageSrc: 'murder_in_paris_preview',
-      tags: ['Profession', 'Frontend', 'Angular','Firebase', 'Web'],
+      tags: ['Profession', 'Frontend', 'Angular','Firebase'],
       liveUrl: 'https://murderinparis.com/',
       platforms: ['Web']
     }
@@ -66,5 +60,6 @@ export interface Project {
     repoLink: string;
   };
   liveUrl?: string;
+  sourceCode?: string;
   platforms?: string[];
 }
