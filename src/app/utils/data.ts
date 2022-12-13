5e38f0e301a170a97ebd410db99b47e99f8c6bba
Tanaka <tanakakenne2000@gmail.com>
2022-12-13 19:26:20 +0200

    fixed data

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -69,7 +69,7 @@ export const PROJECTS:Project[] = [
     {
       id: 'mentor_me',
       name: 'Mentor Me',
-      description: 'This is a mobile application that seeks to provide value through mentorship.',
+      description: 'A mobile application that seeks to provide value through mentorship.',
       imageSrc: 'mentor_me_preview',
       tags: ['Frontend','Firebase', 'Ionic', 'Node.js', 'Backend', 'Neo4j', 'Personal'],
       platforms: ['Mobile'],
@@ -80,7 +80,7 @@ export const PROJECTS:Project[] = [
     {
       id: 'assembly_management',
       name: 'Assembly Management',
-      description: 'platform for the AFM Church in the Gauteng Region that attempts to modernize and/or digitize the existing manual management system currently being used by the church.',
+      description: 'A platform for the AFM Church in the Gauteng Region that attempts to modernize and/or digitize the existing manual management system currently being used by the church.',
       tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Node.js', 'Backend'],
       platforms: ['Web'],
       team: getTeam(['migal'])
@@ -88,7 +88,7 @@ export const PROJECTS:Project[] = [
     {
       id: 'horus',
       name: 'Horus Management System',
-      description: 'remote asset monitoring platform for cell towers that provides clients with visibility of their entire asset portfolio',
+      description: 'A remote asset monitoring platform for cell towers that provides clients with visibility of their entire asset portfolio',
       tags: ['Professional', 'Frontend', 'Angular','Firebase', 'Ionic', 'Node.js', 'Backend'],
       liveUrl: '',
       platforms: ['Web', 'Mobile'],
@@ -97,7 +97,7 @@ export const PROJECTS:Project[] = [
     {
       id: 'kasi_connect',
       name:'Kasi Connect',
-      description: 'A mobile that attempted to bring internet connect to South African townships',
+      description: 'A mobile application that attempted to bring internet connect to South African townships',
       imageSrc: 'kasi_connect_preview',
       tags: ['Profession', 'Frontend', 'Angular','Firebase', 'Ionic'],
       liveUrl: 'https://play.google.com/store/apps/details?id=za.co.omnipresent.kasi.connect&pli=1',
