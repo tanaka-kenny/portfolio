7ef83ff1cd50f2d3cf1a0f6389b3e9fb9086eee9
Tanaka <tanakakenne2000@gmail.com>
2023-01-16 13:15:48 +0200

    removed contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 
@@ -14,10 +13,6 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
-  },
-  {
-    path: 'contact',
-    component: ContactComponent
   },
   {
     path: 'projects',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DetailComponent } from './pages/detail/detail.component';
@@ -16,7 +15,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    ContactComponent,
     ProjectsComponent,
     FooterComponent,
     DetailComponent
